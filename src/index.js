753ca1cf0924fe079a69706c93f63b4f370dc509
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-02-12 19:28:31 -0600

    react app now converted to react-redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router,Route} from 'react-router-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
+
+import App from "./App";
+import reducers from "./reducers";
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-      <Router>
-          <Route path="/" component={App} />
-      </Router>, 
-      document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
