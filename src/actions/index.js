aef10fa370adaa9bec3f0a8eed7dc0dbb07fdc82
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-02-18 22:07:07 -0600

    created memoized action fetchSingleRestaurant();

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,6 +12,22 @@ const _registerUser = _.memoize(async (registrationInfo, dispatch) => {
     dispatch({ type: "REGISTER_USER", payload: userInfo });
 });
 
+const _fetchSingleRestaurant = _.memoize(
+  async (restaurant_api_key, dispatch) => {
+    const response = await apiConnector.getIndividualRestaurant(
+      restaurant_api_key
+    );
+    const restaurantResponse = response.data;
+
+    if (response.staus.code === 200) {
+      dispatch({
+        type: "RECIEVE_SINGLE_RESTAURANT",
+        payload: restaurantResponse
+      });
+    }
+  }
+);
+
 /* --- functions that execute --- */
 
 export const listRestaurants = () => async dispatch => {
@@ -21,6 +37,10 @@ export const listRestaurants = () => async dispatch => {
 
 export const registerUser = registrationInfo => dispatch =>
   _registerUser(registrationInfo, dispatch);
+
+export const fetchSingleRestaurant = restaurant_api_key => async dipatch => {
+  _fetchSingleRestaurant(restaurant_api_key, dipatch);
+};
 
 export const logoutUser = user => {
   return {
