4ad16c9814aca964bf0d08529d89f231f8fce3a8
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-02-18 21:58:00 -0600

    created case for fetching single restaurant from api

--- src/reducers/restaurantReducer.js
+++ src/reducers/restaurantReducer.js
@@ -2,6 +2,8 @@ export default function restaurantReducer(state = [], action) {
   switch (action.type) {
     case "LIST_RESTAURANTS":
       return action.payload;
+    case "RECIEVE_SINGLE_RESTAURANT":
+      return action.payload;
     default:
       return state;
   }
