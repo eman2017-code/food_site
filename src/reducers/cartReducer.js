30d0e386b8d0276b1ef49a4e18975a3ca630d0db
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-02-24 10:17:04 -0600

    created remove/update cart cases in cartReducer

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -3,11 +3,34 @@ const initialState = {
 };
 
 export default function cartReducer(state = initialState, action) {
+  let cart = state.cart;
   switch (action.type) {
     case "ADD_TO_CART":
       return {
         ...state,
         cart: action.payload
+      };
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: cart.filter(
+          item => item.product.apiKey != action.payload.productApiKey
+        )
+      };
+    case "UPDATE_CART_QUANTITY":
+      let item = cart.find(item => item.product.id == action.payload.productId);
+
+      let newCart = cart.filter(
+        item => item.product.id != action.payload.productId
+      );
+
+      item.quantity = action.payload.quantity;
+
+      newCart.push(item);
+
+      return {
+        ...state,
+        cart: newCart
       };
     case "CLEAR_CART":
       return {
