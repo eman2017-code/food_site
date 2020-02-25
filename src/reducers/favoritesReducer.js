2ca9f11520fc261ab65bed53db32cd505ffb922d
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-24 18:06:58 -0600

    created case in reducer to set users favorite foods in the store

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -8,6 +8,13 @@ export default function favoritesReducer(state = initialState, action) {
 
     switch (action.type) {
 
+        // sets all of the users favorite foods
+        case 'SET_FAVORITE_FOODS':
+            return {
+                ...state,
+                favoriteFoods: action.payload
+            }
+
         default:
             return state
     }
