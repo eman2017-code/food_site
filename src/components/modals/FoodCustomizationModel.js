af632c969d257bb0a57e786b7d3bd42ea1079f75
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-25 12:39:44 -0600

    makes fetch call to get food item customizations

--- src/components/modals/FoodCustomizationModel.js
+++ src/components/modals/FoodCustomizationModel.js
@@ -7,6 +7,28 @@ class FoodCustomizatonModal extends React.Component {
 
     constructor(props) {
         super(props)
+
+        this.state = {
+            customizationOptions: {}
+        }
+        console.log('foodItem:', this.props.foodItem);
+    }
+
+    componentDidMount() {
+        this.getCustomizationOptions();
+    }
+
+    // gets all of the customization options for the food item
+    getCustomizationOptions = async () => {
+        const apiURL = 'http://localhost:8000/api/v1/food-item-customizations/?food_item_api_key=' + 
+                        this.props.foodItem.apiKey
+
+        const response = await fetch(apiURL, {
+            method: 'GET',
+            credentials: 'include'
+        });
+        const parsedResponse = await response.json();
+        console.log('response:', parsedResponse);
     }
 
     render() {
@@ -22,9 +44,11 @@ class FoodCustomizatonModal extends React.Component {
                     <Modal.Title as='h5' id="edit-profile">{ foodItem.name }</Modal.Title>
 			    </Modal.Header>
                 <Modal.Body>
-                    <p className="text-gray">
-                        { foodItem.description }
-                    </p>
+                    <p className="text-gray">{ foodItem.description }</p>
+
+                    <Form>
+                        
+                    </Form>
                 </Modal.Body>
             </Modal>
         )
