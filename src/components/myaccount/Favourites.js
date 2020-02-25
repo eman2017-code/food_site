868f08e6a9f0cbba68635d3ff783d1fe08010985
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-24 18:12:10 -0600

    component display favorite foods in the store

--- src/components/myaccount/Favourites.js
+++ src/components/myaccount/Favourites.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { getUsersFavoriteFoods } from '../../actions';
 import {Row,Col,Button,Spinner} from 'react-bootstrap';
 import CardItem from '../common/CardItem';
 
 class Favourites extends React.Component {
+
+	componentDidMount() {
+		this.props.getUsersFavoriteFoods();
+		console.log('initial favorite foods:', this.props.favoriteFoods);
+	}
 
 	render() {
     	return (
@@ -14,24 +20,32 @@ class Favourites extends React.Component {
 	                 <Col md={12}>
 	                    <h4 className="font-weight-bold mt-0 mb-3">Favourites</h4>
 	                 </Col>
-	                 <Col md={4} sm={6} className="mb-4 pb-2">
-	                    <CardItem 
-					   		title='Bite Me Sandwiches'
-							subTitle='North Indian • American • Pure veg'
-						  	imageAlt='Product'
-						    image='img/list/1.png'
-						    imageClass='img-fluid item-img'
-						    linkUrl='detail'
-						    offerText='65% off Coupon OSAHAN50'
-						    offerColor='danger'
-							time='15–25 min'
-							price='$100 FOR TWO'
-							showPromoted={true}
-							promotedVariant='dark'
-							favIcoIconColor='text-danger'
-							rating='3.1 (300+)'
-					   	/>
-	                 </Col>
+					 {
+						 this.props.favoriteFoods.map(food => {
+							return (
+								<Col md={4} sm={6} key={food.id} className="mb-4 pb-2">
+	                    			<CardItem 
+					   					title='Bite Me Sandwiches'
+										subTitle='North Indian • American • Pure veg'
+						  				imageAlt='Product'
+						    			image='img/list/1.png'
+						    			imageClass='img-fluid item-img'
+						    			linkUrl='detail'
+						    			offerText='65% off Coupon OSAHAN50'
+						    			offerColor='danger'
+										time='15–25 min'
+										price='$100 FOR TWO'
+										showPromoted={true}
+										promotedVariant='dark'
+										favIcoIconColor='text-danger'
+										rating='3.1 (300+)'
+					   				/>
+	                 			</Col>
+
+							)
+						 })
+					 }
+	                 
 	              </Row>
 			    </div>
 		    </>
@@ -45,4 +59,4 @@ const mapStateToProps = state => {
 	}
 }
 
-export default connect(mapStateToProps, {})(Favourites);
+export default connect(mapStateToProps, { getUsersFavoriteFoods })(Favourites);
